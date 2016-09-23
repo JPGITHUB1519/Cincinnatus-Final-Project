3015ce8a6f2b9e68037c01efaf33a2598b161d29
Jean Ureña <juanpedro1519@gmail.com>
2016-09-23 15:48:58 -0400

    Perphap

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,7 @@ var myChart = new Chart(ctx, {
     data: {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
-            label: '# of Votes',
+            label: '# Post by Topic',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
