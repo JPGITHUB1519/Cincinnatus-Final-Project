d01df28c87ae387b2d237487d84d389256ff9d9e
Fabian <samus0226@hotmail.com>
2016-10-29 13:17:53 -0400

    Delete Comment Admin

--- handlers/admin_comments_list_handler.py
+++ handlers/admin_comments_list_handler.py
@@ -1,4 +1,5 @@
-from basic_handler import * 
+from basic_handler import *
+import json 
 
 class AdminCommentsListHandler(Handler) :
 	def get(self, post_id) :
@@ -7,3 +8,17 @@ class AdminCommentsListHandler(Handler) :
 		post = get_permalink(post_id)
 		logging.error(comments)
 		self.render("admin_comment_list.html", p = post, list_comments = comments)
+
+	def post(self, post_id) :
+		self.response.headers['Content-Type'] = 'application/json'
+		data = json.loads(self.request.body);
+		action = data["action"]
+		comment_id = data["comment_id"]
+		data["status"] = "ok"
+
+		if action == "delete_comment" :
+			comment_entity = comment_by_id(comment_id)
+			if comment_entity :
+				comment_entity.delete()
+				get_comments_by_post(post_id, True)
+		self.write(json.dumps(data))
